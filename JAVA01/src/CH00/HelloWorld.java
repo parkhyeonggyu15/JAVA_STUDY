c59a0b39ff4a04d3a971e43fb916cc2ab34a7b11
parkhyeonggyu15 <gudrb15@naver.com>
2026-04-16 17:26:13 +0900

    01DAY-JAVA

---
+++ JAVA01/src/CH00/HelloWorld.java
@@ -0,0 +1,23 @@
+package CH00;	// 패키지명
+
+public class HelloWorld // 클래스 영역 - 객체지향문법 적용영역
+{
+
+//	메서드(함수)종류
+//	라이브러리 메서드 		: 미리 만들어져 제공되는 메서드
+//	사용자정의 메서드		: 개발자에 의해 만들어지는 메서드
+//	main 메서드			: 최초 실행되는 메서드
+	
+	public static void main(String[] args) // main메서드 선언부 - 절차지향문법 적용영역
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World"); // 라이브러리 메서드
+		
+	}
+	
+	// 글자크기 조정 : ctrl + '+' or '-'
+	// 주석처리 : ctrl + '/'
+	// 한줄복사 : ctrl + alt + '↓'
+	// 한줄삭제 : ctrl + d
+	
+}
