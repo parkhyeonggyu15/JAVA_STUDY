e1f9a10e7f0b82790eea79ba2ecf676135ead289
parkhyeonggyu15 <gudrb15@naver.com>
2026-04-17 12:31:47 +0900

    02DAYS-JAVA

---
+++ JAVA01/src/CH01/CH04변수_자료형.java
@@ -0,0 +1,30 @@
+package CH01;
+
+public class CH04변수_자료형 {
+
+	public static void main(String[] args) {
+
+//		byte(1byte - signed)
+//		byte(2byte - unsigned)
+//		short(2byte - signed)
+//		int(4byte - signed)
+//		float(4byte)
+//		double(8byte)
+		
+		int num1;					//	int만큼 크기(4byte)의 공간형성 + num1이름부여(변수 정의)
+		num1 = 10;					//	10이라고 하는값(리터럴상수)을 상수POOL에 저장,num1공간에 대입(복사)
+		int num2 = 4;				//	4라는 값(리터럴상수)을 상수POOL저장, 4byte 정수공간 num2 초기화
+		int num3 = num1 + num2;		//	num1안의값과 num2안의값의 덧셈결과(CPU가산처리)를 4byte 정수공간 num3에 초기화
+		System.out.println(num3);	//	num3안의 값을 println매서드로 전달해서 내부적으로 표준출력 처리
+		
+		// Data (수,자료) : 선 저장 / 후 처리
+		// 변수 : 개발자의 유지보수 측면에서 유리하도록 하기 위해 지정한 수 (바뀔 예정인 수)
+		// 변수명 : 저장되어져 있는 변수 공간에 접근하기 위한 문자 형태의 주소
+		// 자료형 : Data(수,자료)를 저장하기 위한 공간을 형성하고 저장될 자료의 형태를 제한하는 예약어
+		// =연산자 :
+		// lv(공간) = rv(값) rv를 먼저 처리(저장 or 연산) 한 다음 lv에 대입
+		
+		
+	}
+
+}
---
+++ JAVA01/src/CH01/CH05변수_자료형.java
@@ -0,0 +1,136 @@
+package CH01;
+
+public class CH05변수_자료형 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//----------------------------
+		// 정수 int - 4byte 정수 부호 o
+//		//----------------------------
+//		int n1 = 0b10101101;
+//		int n2 = 173;		//10진정수값
+//		int n3 = 0255;		//8진수
+//		int n4 = 0xad;		//16진수
+//		System.out.printf("%d %d %d %d\n",n1,n2,n3,n4);
+		
+		//----------------------------
+		// 정수 byte - 1byte 정수 부호 o
+//		//----------------------------
+//		byte n5 = (byte)-129;		//?
+//		byte n6 = -30;
+//		byte n7 = 30;
+//		byte n8 = 127;
+//		byte n9 = (byte)129;		//?
+//		System.out.println("n6 : "+ n6);
+//		System.out.println("n9 : "+ n9);
+
+		//--------------------------------
+		//정수 short-2byte 정수 부호o | char-2byte 정수 부호x(양수만)
+		//---------------------------------
+//		char n1 = 65535;	// (0~2^16-1) (0~65535)
+//		short n2 = 32767;	// (-2&15 ~ 2^15-1)(-32768 ~ + 32767)
+//		
+//		System.out.printf("n1 : " + n1 );	//왜 0이 나올까?
+//
+//		short n4 = (short)n1; //문제발생...왜? 
+//		System.out.printf("%d\n",n4);
+//		System.out.println(Integer.toBinaryString(65535));
+//		System.out.printf("%d\n",n4);
+		
+		//--------------------------------
+		//정수 long-8byte 정수 부호o
+		//---------------------------------
+//		long n1 = 2150000000L; //10억
+//		long n2 = 20;	//L,l (리터럴접미사) : long 자료형 사용하여 값 저장
+//		
+//		long n3 = 10000000000l;	//문제발생.. 왜?
+//		long n4 = 10000000000L;
+		
+		//---------------------------------
+		//실수
+		//---------------------------------
+		//유리수와 무리수의 통칭
+		//소숫점이하값을 가지는 수 123.456
+		//float : 4byte 실수(6-9자리)
+		//double : 8byte 실수(15-18자리),기본자료형
+		
+		//정밀도 확인
+//		float n1 = 0.123456789123456789F; //f,F:float형 접미사
+//		double n2 = 0.123456789123456789;
+//		
+//		System.out.println(n1);
+//		System.out.println(n2);
+	
+		//오차 확인
+//		float num = 0.1f;
+//		for(int i=0;i<=1E5;i++) {
+//				num=num+0.1f;
+//				System.out.println(i);
+//		}
+//		System.out.println("num : " + num);
+
+		//----------------------------
+		// 단일문자 char 2byte 정수
+		// ---------------------------
+//		char ch1 = 'a';
+//		System.out.println(ch1);
+//		System.out.println((int)ch1);
+//		System.out.println(Integer.toBinaryString(ch1));
+//		System.out.println("--------------------------");
+//
+//		char ch2 = 98;
+//		System.out.println(ch2);
+//		System.out.println((int)ch2);
+//		System.out.println(Integer.toBinaryString(ch1));
+//		System.out.println("--------------------------");
+//
+//		char ch3 = 'b' + 1;
+//		System.out.println(ch3);
+//		System.out.println((int)ch3);
+//		System.out.println(Integer.toBinaryString(ch3));
+//		
+//		System.out.println((char)0b10101100000000000);
+//		char ch4 = 0xac02;
+//		System.out.println(ch4);
+//		System.out.println((int)ch4);
+//		System.out.println(Integer.toBinaryString(ch4));
+//		
+//		System.out.println("--------------------------");
+//		// \\u : 유니코드 이스케이프문자
+//		System.out.printf("%c\n", 0xac03);
+//		System.out.printf("%c\n", '\uac03');
+		
+		//---------------------------------
+		//boolean : 논리형(true/false 저장)
+		//---------------------------------
+//		boolean flag = (10>11);		// 참(긍정)
+//		if(flag)
+//		{
+//			System.out.println("참인경우 실행");
+//		}
+//		else
+//		{
+//			System.out.println("거짓인경우 실행");
+//		}
+		
+		//---------------------------
+		//문자열 : String (클래스자료형)
+		//---------------------------
+		//기본자료형(원시타입)
+		//byte n1;
+		//short n2;
+		//double n3;
+		//long n4;
+		
+		//클래스자료형
+		//클래스자료형으로 만든변수는 '참조변수'라고 하고
+		//참조변수는 데이터가 저장된 위치정보(메모리주소값)이 저장된다.
+		int n1 = 10;
+		byte n2 = 20;
+		char n3 = 40;
+		
+		String name = "홍길동";
+		String job = "프로그래머";
+	}
+
+}
