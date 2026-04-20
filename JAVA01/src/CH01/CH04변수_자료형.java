98b1564620c38db49ebb0db8a2b96db3d86f11f8
parkhyeonggyu15 <gudrb15@naver.com>
2026-04-20 12:34:54 +0900

    02DAYS-JAVA

--- JAVA01/src/CH01/CH04변수_자료형.java
+++ JAVA01/src/CH01/CH04변수_자료형.java
@@ -11,11 +11,11 @@ public class CH04변수_자료형 {
 //		float(4byte)
 //		double(8byte)
 		
-		int num1;					//	int만큼 크기(4byte)의 공간형성 + num1이름부여(변수 정의)
-		num1 = 10;					//	10이라고 하는값(리터럴상수)을 상수POOL에 저장,num1공간에 대입(복사)
-		int num2 = 4;				//	4라는 값(리터럴상수)을 상수POOL저장, 4byte 정수공간 num2 초기화
-		int num3 = num1 + num2;		//	num1안의값과 num2안의값의 덧셈결과(CPU가산처리)를 4byte 정수공간 num3에 초기화
-		System.out.println(num3);	//	num3안의 값을 println매서드로 전달해서 내부적으로 표준출력 처리
+//		int num1;					//	int만큼 크기(4byte)의 공간형성 + num1이름부여(변수 정의)
+//		num1 = 10;					//	10이라고 하는값(리터럴상수)을 상수POOL에 저장,num1공간에 대입(복사)
+//		int num2 = 4;				//	4라는 값(리터럴상수)을 상수POOL저장, 4byte 정수공간 num2 초기화
+//		int num3 = num1 + num2;		//	num1안의값과 num2안의값의 덧셈결과(CPU가산처리)를 4byte 정수공간 num3에 초기화
+//		System.out.println(num3);	//	num3안의 값을 println매서드로 전달해서 내부적으로 표준출력 처리
 		
 		// Data (수,자료) : 선 저장 / 후 처리
 		// 변수 : 개발자의 유지보수 측면에서 유리하도록 하기 위해 지정한 수 (바뀔 예정인 수)
