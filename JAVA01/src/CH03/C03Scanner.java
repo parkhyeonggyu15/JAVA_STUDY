bd6af66046655bb10df92e3a319e7ec933381c1b
parkhyeonggyu15 <gudrb15@naver.com>
2026-04-21 11:34:43 +0900

    04DAYS-JAVA

--- JAVA01/src/CH03/C03Scanner.java
+++ JAVA01/src/CH03/C03Scanner.java
@@ -6,18 +6,26 @@ public class C03Scanner {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		// 이름을 입력하세요? 홍길동
+		System.out.print("이름을 입력하세요?");
+		String name = sc.next();
 		
-		System.out.print("홍길동 님의 나이를 입력하세요? ");
-		int num1 = sc.nextInt();
-		
-		sc.nextLine();
-		System.out.print("홍길동 님의 주소를 입력하세요? ");
-		String adr1 = sc.nextLine();
-		System.out.println("홍길동 님의 나이는 " +num1 + "세"+ " 주소는 " + adr1 + " 입니다");
-		sc.close();
 		// 홍길동 님의 나이를 입력하세요? 35
+		System.out.printf("홍길동 님의 나이를 입력하세요? ",name);
+		int age = sc.nextInt();
+		
 		// 홍길동 님의 주소를 입력하세요? 대구광역시 달서구 000
+		sc.nextLine();
+		System.out.printf("홍길동 님의 주소를 입력하세요? ",name);
+		
 		// 홍길동 님의 나이는 35세 주소는 대구광역시 ~~ 입니다	
+		String address = sc.nextLine();
+		System.out.printf("%s 님의 나이는 %d세 주소는 %s 입니다\n", name,age,address);
+		sc.close();
+	
+		
+		
+		
 	}
 
 }
---
+++ JAVA01/src/CH04/C02Ex.java
@@ -0,0 +1,43 @@
+package CH04;
+
+import java.util.Scanner;
+
+public class C02Ex {
+
+	public static void main(String[] args) {
+		//1.문제(삼항연산자 + 논리연산자)
+		// 키보드로 국어/영어/수학 점수를 입력받아 각각의부분점수가 40점이상이고
+		// 총점수합의 평균이 70점이상이면 '합격'
+		// 미만이면 '불합격'을 출력합니다
+//		Scanner sc = new Scanner(System.in);
+//		int kor = sc.nextInt();
+//		int eng = sc.nextInt();
+//		int math = sc.nextInt();
+//		int sum = (kor + eng + math);
+//		double avg = sum/3;
+//		
+//		String grade = (kor>=40 && eng>=40 && math>=40 && avg>=70 ? "합격" : "불합격");
+//		System.out.println(grade);
+//		
+//		sc.close();
+		
+
+
+		//2.미니문제(%연산자) - N%2==0
+		//키보드로 부터 정수값 2개를 입력받아
+		//두수의 합이 짝수이면 "짝수입니다"
+		//홀수이면 "홀수입니다" 를 출력하세요~
+
+//		Scanner sc = new Scanner(System.in);
+//		int n1 = sc.nextInt();
+//		int n2 = sc.nextInt();
+//		
+//		String num = (n1 + n2)%2==0 ? "짝수입니다" : "홀수입니다";
+//		System.out.println(num);
+//		
+//		sc.close();
+
+
+	}
+
+}
