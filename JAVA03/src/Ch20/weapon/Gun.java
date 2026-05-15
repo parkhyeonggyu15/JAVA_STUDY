c3ce78292373eb3ecbd8a964eca75fb360ccb021
parkhyeonggyu15 <gudrb15@naver.com>
2026-05-15 17:13:22 +0900

    18DAYS-JAVA

---
+++ JAVA03/src/Ch20/weapon/Gun.java
@@ -0,0 +1,14 @@
+package Ch20.weapon;
+
+import Ch20.unit.Unit;
+
+public abstract class Gun {
+	
+	int maxBuillitCnt;
+	int curBuillitCnt;
+	int power;
+	
+	abstract public void fire(Unit unit);
+	abstract public void reload(int bullit);
+
+}
